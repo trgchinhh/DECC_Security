53219ecddce7834e45ab94a9c60cf462b43870ad
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2025-05-02 15:24:19 +0700

    Delete DECC/Code_mau.py

--- DECC/Code_mau.py
+++
@@ -1,8 +0,0 @@
-# Đây là 1 đoạn code mẫu thử nghiệm 
-
-def hello(*, name : str, tuoi : int):
-    print(f"Xin chao {name} {tuoi} tuổi")
-
-name = "Nguyen Truong Chinh"
-tuoi = 19
-hello(name = name, tuoi = tuoi)    
